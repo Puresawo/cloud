c697baa5186dcf08c2e144a0ef861bd8e1532145
Puresawo <99786427+Puresawo@users.noreply.github.com>
2025-07-03 10:46:57 +0300

    Delete SetLangList2.ps1

--- SetLangList2.ps1
+++
@@ -1,7 +0,0 @@
-
-###########################################
-# PowerShell Script for users or login script
-###########################################
-$prefered_list = Get-WinUserLanguageList
-$prefered_list.Add("fi-fi")
-Set-WinUserLanguageList($prefered_list) -Force
